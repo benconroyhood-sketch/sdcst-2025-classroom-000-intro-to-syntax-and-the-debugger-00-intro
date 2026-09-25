78df4a1c8dbf9a4aca2624096c0084c3b6c16ab6
benconroyhood-sketch <ben.conroyhood@icloud.com>
2026-09-25 08:53:28 -0700

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,5 +1,17 @@
 #!python3
 
-print("this is the first line")
-    print("this is the second line")
+#print("this is the first line")
+print("this is the second line")
 print("this is the third line")
+print("this is a change so i can push and fork it")
+print("she sells sea shells on the sea shore")
+
+"""
+print("This code is commented so shouldn't be parsed")
+I can put anything I want here.
+Fake HTML:
+<p>hello there</p>
+<br>
+<br>
+<p>goodbye</p>
+"""
